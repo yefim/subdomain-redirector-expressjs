289a143892361e0f30b75a4b80afc85ba01c90d3
Yefim Vedernikoff <yefim323@gmail.com>
2022-11-28 01:53:06 -0500

    Make redirects work

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 import express from 'express';
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 const app = express();
 
 app.get('/', async (req, res) => {
-  res.send('redirecting to hello world');
+  const link = await prisma.shortLink.findUnique({
+    where: {
+      host: req.hostname,
+    },
+  });
+
+  if (link) {
+    res.redirect(link.redirectUrl);
+  } else {
+    res.send(`link does not exist for ${req.hostname}`);
+  }
 });
 
 app.get('/edit', async (req, res) => {
+  const link = await prisma.shortLink.create({
+    data: {
+      host: req.hostname,
+      redirectUrl: 'https://google.com',
+    },
+  });
+
+  res.send(`you are editing ${req.hostname}`);
 });
 
 app.post('/edit', async (req, res) => {
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening...');
+});
