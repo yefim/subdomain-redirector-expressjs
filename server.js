bc5bbcf456a13d0b33b309fe85514e41a64d933e
Yefim Vedernikoff <yefim323@gmail.com>
2022-11-29 02:38:35 -0500

    Implement creating and editing links

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', async (req, res) => {
   const link = await prisma.shortLink.findUnique({
@@ -12,24 +14,55 @@ app.get('/', async (req, res) => {
   });
 
   if (link) {
-    res.redirect(link.redirectUrl);
+    // res.redirect(link.redirectUrl);
+    console.log(req.headers);
+    // log the hit
+    // const hit = await prisma.hit.create({data: {}});
+
+    res.send(`redirecting to ${link.redirectUrl}`);
   } else {
     res.send(`link does not exist for ${req.hostname}`);
   }
 });
 
 app.get('/edit', async (req, res) => {
-  res.send(`you are editing ${req.hostname}`);
+  const link = await prisma.shortLink.findUnique({
+    where: {
+      host: req.hostname,
+    },
+  });
+
+  res.send(`<form method="post" action="" onsubmit="navigator.clipboard.writeText('https://' + window.location.host)">
+      <label>Redirect URL
+        <input type="text" name="redirectUrl" value="${link.redirectUrl || ''}">
+      </label>
+      <label>Password
+        <input type="password" name="password">
+      </label>
+      <button type="submit">Submit</button>
+    </form>`);
 });
 
 app.post('/edit', async (req, res) => {
-  if (req.body.password && req.body.password === 'TESTING') {
-    const link = await prisma.shortLink.create({
-      data: {
-        host: req.hostname,
-        redirectUrl: req.body.redirectUrl,
+  const hostname = req.hostname;
+  const {
+    redirectUrl,
+    password,
+  } = req.body;
+
+  if (redirectUrl && password === 'TESTING') {
+    const link = await prisma.shortLink.upsert({
+      where: {
+        host: hostname,
+      },
+      update: { redirectUrl },
+      create: {
+        host: hostname,
+        redirectUrl,
       },
     });
+
+    res.redirect(link.redirectUrl);
   } else {
     res.send('wrong password');
   }
